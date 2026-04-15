5f5f23cd9de1c9500bfe1e158e03a48ef6a22c97
Ferreira <jose.chavez@iteso.mx>
2026-04-14 22:59:10 -0600

    fix(celery): autodiscover lazy para evitar import circular con settings

--- badgeup/celery.py
+++ badgeup/celery.py
@@ -6,4 +6,11 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "badgeup.settings")
 
 app = Celery("badgeup")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
+
+
+def _installed_apps():
+    from django.conf import settings
+    return list(settings.INSTALLED_APPS)
+
+
+app.autodiscover_tasks(_installed_apps)
